e6ae66296e2881ba19bda19ba6d7f340b39195d1
Jean-Alain Grunchec <jagrunchec@hotmail.com>
2022-01-30 12:44:11 +0100

    Start project

---
+++ src/main/java/com/machin/hsbc/ExceptionManagerUiApplication.java
@@ -0,0 +1,13 @@
+package com.machin.hsbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionManagerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionManagerUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/machin/hsbc/ExceptionManagerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machin.hsbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionManagerUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
